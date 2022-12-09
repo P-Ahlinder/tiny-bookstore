bf75c0d30a3b892f70a2d97122bc957501832761
Simon <backstrom94+github@gmail.com>
2022-12-09 16:13:43 +0100

    Added BookListItem component

--- client/src/components/BookListItem.tsx
+++ client/src/components/BookListItem.tsx
@@ -1,5 +1,40 @@
-function BookListItem() {
-  return <div>BookListItem</div>;
+import { gql, useMutation } from "@apollo/client";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Book } from "../types";
+
+type Props = {
+  book: Book;
+};
+
+const REMOVE_BOOK = gql`
+  mutation removeBook($removeBookId: ID) {
+    removeBook(id: $removeBookId) {
+      id
+      title
+    }
+  }
+`;
+
+function BookListItem({ book }: Props) {
+  const [removeBook, { data, loading, error }] = useMutation(REMOVE_BOOK);
+
+  // TODO: Add logic to remove book!
+  const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(book.title);
+  };
+
+  return (
+    <article>
+      <Link to={`/book/${book.id}`}>
+        <h2>{book.title}</h2>
+      </Link>
+      <Link to={`/author/${book.author.id}`}>
+        <p>{book.author.name}</p>
+      </Link>
+      <button onClick={handleRemove}>Remove</button>
+    </article>
+  );
 }
 
 export default BookListItem;
--- client/src/pages/BookList.tsx
+++ client/src/pages/BookList.tsx
@@ -1,6 +1,6 @@
-import { useQuery, gql, useMutation } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import BookListItem from "../components/BookListItem";
 import { Book } from "../types";
 
 const GET_BOOKS = gql`
@@ -17,24 +17,10 @@ const GET_BOOKS = gql`
   }
 `;
 
-const REMOVE_BOOK = gql`
-  mutation removeBook($removeBookId: ID) {
-    removeBook(id: $removeBookId) {
-      id
-      title
-    }
-  }
-`;
-
 function Books() {
-  // const [removeBook, { data, loading, error }] = useMutation(REMOVE_BOOK);
   const { loading, error, data } = useQuery(GET_BOOKS);
   const [filteredBooks, setFilteredBooks] = useState([]);
   const searchInputRef = useRef<HTMLInputElement>(null);
-
-  // useEffect(() => {
-  //   setFilteredBooks(data.books);
-  // }, []);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const searchResult = data.books.filter((book: Book) => {
@@ -46,8 +32,6 @@ function Books() {
     setFilteredBooks(searchResult);
   };
 
-  const handleRemove = (e: React.MouseEvent<HTMLButtonElement>) => {};
-
   const getBooksArray = () => {
     if (searchInputRef.current != null && searchInputRef.current.value.length > 1) {
       return filteredBooks;
@@ -57,8 +41,6 @@ function Books() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-
-  // setFilteredBooks([...data.books]);
 
   return (
     <div>
@@ -74,15 +56,7 @@ function Books() {
       </form>
       <h1>Books: </h1>
       {getBooksArray().map((book: Book) => (
-        <article>
-          <Link to={`/book/${book.id}`} key={book.id}>
-            <h2>{book.title}</h2>
-          </Link>
-          <Link to={`/author/${book.author.id}`} key={"author" + book.id}>
-            <p>{book.author.name}</p>
-          </Link>
-          <button>Remove</button>
-        </article>
+        <BookListItem book={book} key={book.id} />
       ))}
     </div>
   );
